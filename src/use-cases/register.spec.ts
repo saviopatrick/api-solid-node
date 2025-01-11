dfb47fb1ea97e12ce89b19c130c648aa20739a8d
saviopatrick <saviopatrick09@gmail.com>
2025-01-11 18:33:08 -0300

    feat: improve user registration process

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,47 @@
-import { expect, test } from 'vitest'
+import { describe, it, expect } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '../repositores/in-memory/in-memory-users-repository'
+import { Conflit } from '../errors/conflit'
 
-test('check if it works', () => {
-  expect(1 + 1).toBe(2)
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const usersRepositorys = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepositorys)
+    const { user } = await registerUseCase.execute({
+      email: 'jhondoe@example.com',
+      name: 'John Doe',
+      password: '123456',
+    })
+    expect(user.id).toEqual(expect.any(String))
+  })
+  it('should hash user password upon registration', async () => {
+    const usersRepositorys = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepositorys)
+    const { user } = await registerUseCase.execute({
+      email: 'jhondoe@example.com',
+      name: 'John Doe',
+      password: '123456',
+    })
+    const isPasswordHashed = await compare('123456', user.password_hash)
+    expect(isPasswordHashed).toBe(true)
+  })
+  it('should not allow two users to register with the same email', async () => {
+    const usersRepositorys = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepositorys)
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      email,
+      name: 'John Doe',
+      password: '123456',
+    })
+    expect(async () => {
+      await registerUseCase.execute({
+        email,
+        name: 'John Doe',
+        password: '123456',
+      })
+    }).rejects.toBeInstanceOf(Conflit)
+  })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,17 +1,25 @@
 import { hash } from 'bcryptjs'
 import { UsersRepository } from '../repositores/users-repository'
 import { Conflit } from '../errors/conflit'
+import type { User } from '@prisma/client'
 
 interface RegisterUserParams {
   email: string
   password: string
   name: string
 }
+interface RegisterUseCaseProps {
+  user: User
+}
 
 export class RegisterUseCase {
   constructor(private usersRepositorys: UsersRepository) {}
 
-  async execute({ email, password, name }: RegisterUserParams) {
+  async execute({
+    email,
+    password,
+    name,
+  }: RegisterUserParams): Promise<RegisterUseCaseProps> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepositorys.findByEmail(email)
@@ -19,10 +27,13 @@ export class RegisterUseCase {
       throw new Conflit('Email already in use')
     }
 
-    await this.usersRepositorys.create({
+    const user = await this.usersRepositorys.create({
       email,
       password_hash,
       name,
     })
+    return {
+      user,
+    }
   }
 }
